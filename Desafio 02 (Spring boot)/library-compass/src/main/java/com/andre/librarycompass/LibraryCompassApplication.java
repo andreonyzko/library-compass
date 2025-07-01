211743294e1af323c08983cfc6f8053ab6cba10f
André Onyszko <145982643+andreonyzko@users.noreply.github.com>
2025-07-01 09:01:11 -0300

    chore: spring boot project setup and initial structure

---
+++ Desafio 02 (Spring boot)/library-compass/src/main/java/com/andre/librarycompass/LibraryCompassApplication.java
@@ -0,0 +1,13 @@
+package com.andre.librarycompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCompassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryCompassApplication.class, args);
+    }
+
+}
