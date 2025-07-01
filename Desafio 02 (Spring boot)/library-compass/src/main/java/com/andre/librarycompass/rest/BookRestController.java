0871e5e4ff16a26f8f0f990b4bd12add44c47635
André Onyszko <145982643+andreonyzko@users.noreply.github.com>
2025-07-01 09:33:54 -0300

    chore(struct): folders organization

--- Desafio 02 (Spring boot)/library-compass/src/main/java/com/andre/librarycompass/rest/BookRestController.java
+++ Desafio 02 (Spring boot)/library-compass/src/main/java/com/andre/librarycompass/rest/BookRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/livros")
+@RequestMapping("/api/livros")
 public class BookRestController {
 
     private BookService bookService;
